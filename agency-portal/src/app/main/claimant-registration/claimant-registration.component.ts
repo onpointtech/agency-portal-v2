48a11978159021f9389a7261bf0a84d6f917ec75
cidazc <joseazcarragaiv@gmail.com>
2019-06-17 21:42:07 +0800

    Make registration form send to backend
    Made the template for registration form correct

--- agency-portal/src/app/main/claimant-registration/claimant-registration.component.ts
+++ agency-portal/src/app/main/claimant-registration/claimant-registration.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { OptimumComponentsModule } from 'optimum-components';
 import { UserProfileModel } from '../optimum-form/user-profile-model';
-import {ClaimantSO} from '../../service-objects/claimant-so'
+import { ClaimantSO } from '../../service-objects/claimant-so'
 import { ReactiveFormsModule, FormControl, FormGroup, FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { ClaimantRegistrationModel } from '../claimant-registration/claimant-registration-model';
@@ -20,39 +20,138 @@ import { ToasterService } from '../../portal-services/toaster.service';
 })
 export class ClaimantRegistrationComponent implements OnInit {
   userProfileModel = new ClaimantSO();
+  addressInitial = new Address();
 
-  radioChoices=['male', 'female', 'prefer not to say',];
-  dropdownChoices = ['yes', 'no', 'maybe'];
+  genderChoices = ['Male', 'Female', 'I prefer not to answer',];
+  raceChoices = [
+  'White',
+  'Black or African American', 
+  'American Indian or Alaska Native', 
+  'Native Hawaiian or other Pacific Islander', 
+  'Asian', 
+  'I prefer not to answer'
+];
+  languagePreferenceChoices = ['English', 'Spanish'];
+  ethnicityChoices = ['Hispanic or Latino', 'Not Hispanic or Latino', 'I prefer not to answer'];
+  educationalLevelChoices = [
+  'Completed 1st Grade', 
+  'Completed 2nd Grade', 
+  'Completed 3rd Grade', 
+  'Completed 4th Grade', 
+  'Completed 5th Grade', 
+  'Completed 6th Grade', 
+  'Completed 7th Grade', 
+  'Completed 8th Grade', 
+  'Completed 9th Grade', 
+  'Completed 10th Grade', 
+  'Completed 11th Grade', 
+  'High School Degree', 
+  'GED or High School Equivalency Diploma',
+  'Some Vocational or Technical School (No Certificate)',
+  'Some College (No Degree)',
+  'Associate\'s Degree',
+  'Bachelor\'s Degree',
+  'Master\'s Degree',
+  'Doctoral Degree',
+  'Post-Doctorate Studies'
+  ];
+
+
 
   profileForm = this.fb.group({
     claimantId: [0, Validators.required],
     ssn: ['', Validators.required],
-    dateofBirth: [new Date(2019,1,1), Validators.required],
     firstName: ['', Validators.required],
-   middleInitial: ['', Validators.required],
     lastName: ['', Validators.required],
+    middleInitial: ['', Validators.required],
+    dateOfBirth: [new Date(2019, 1, 1), Validators.required],
+    homePhone: ['', Validators.required],
+    mobilePhone: ['', Validators.required],
+    preferredOccupation: ['', Validators.required],
+    languagePreference: ['Please select at least one', Validators.required],
+    gender: ['Please select at least one', Validators.required],
+    educationalLevel: ['Please select at least one', Validators.required],
+    race: ['Please select at least one', Validators.required],
+    ethnicity: ['Please select at least one', Validators.required],
+    addressLine1: ['', Validators.required],
+    addressLine2: ['', Validators.required],
+    city: ['', Validators.required],
+    state: ['', Validators.required],
+    zipCode: ['', Validators.required],
+    zipExt: ['', Validators.required],
+
   });
 
 
-  constructor(private fb: FormBuilder, private claimantService: ClaimantService) { 
-    
+  constructor(private fb: FormBuilder, private claimantService: ClaimantService) {
+
   }
 
   ngOnInit() {
-    this.userProfileModel = {claimantId:0, ssn: '123456789', dateOfBirth: new Date('2013-09-22'),firstName: 'Bob',middleInitial: 'D',lastName: 'Ong',homePhone: '', mobilePhone: '', languagePreference: '', educationalLevel: '', gender: '', race: '', ethnicity: '', lastInsertUpdateTS: new Date(2013, 9, 22), lastInsertUpdateBy:'', ivrPin: '', documentDeliveryPreference: '', address: []};
+    this.addressInitial = {
+      id: null,
+      addressLine1: 'Line 1',
+      addressLine2: 'Line 2',
+      state: 'Less',
+      city: 'Owl',
+      zipCode: '8001',
+      zipExt: '1008',
+      lastInsertUpdateTS: new Date(2013, 9, 22),
+      lastInsertUpdateBy: '',
+    },
+
+    this.userProfileModel = { 
+      claimantId: 0, 
+      ssn: '123456789', 
+      dateOfBirth: new Date('2013-09-22'), 
+      firstName: 'Jay', 
+      middleInitial: 'E', 
+      lastName: 'Ibardaloza', 
+      homePhone: '9999999999', 
+      mobilePhone: '99999999998', 
+      languagePreference: 'English', 
+      educationalLevel: 'Doctoral Degree', 
+      gender: 'Male', 
+      race: 'Asian', 
+      ethnicity: 'I prefer not to answer', 
+      lastInsertUpdateTS: new Date(2013, 9, 22), 
+      lastInsertUpdateBy: '', 
+      ivrPin: '', 
+      documentDeliveryPreference: '', 
+      address: [this.addressInitial],
+ };
   }
 
   private http: HttpClient;
   private toasterService: ToasterService
 
-  
+
 
   debug = false;
   submitted = false;
 
-  onSubmit() { 
-    console.log(true);
-    // this.submitted = true;
-    this.claimantService.registerClaimant(this.userProfileModel);  }
+  onSubmit() {
+    this.claimantService
+    .registerClaimant(this.userProfileModel)
+    .then(
+      res => {
+        this.toasterService.success("Success", "Claimant registered successfully");
+      }
+    )
+    .catch(
+      err => {
+        this.toasterService.danger("Error", err);
+      }
+    )
+    
+  }
+
+  thenFn(): any{
+   console.log(true);
+  } 
+
+  compareFn(c1: any, c2: any): boolean {
+    return c1 && c2 ? c1.id === c2.id : c1 === c2;
+  }
 
 }
--- agency-portal/src/app/portal-services/claimant.service.ts
+++ agency-portal/src/app/portal-services/claimant.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http'
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, EMPTY, BehaviorSubject } from 'rxjs';
 import { ClaimantSO } from '../service-objects/claimant-so';
 import { ToasterService } from './toaster.service';
 import { catchError } from 'rxjs/operators'
+
 //import { Claimant } from '../mock-claimant';
 
 
@@ -13,7 +14,6 @@ import { catchError } from 'rxjs/operators'
 export class ClaimantService {
 
   constructor(private http: HttpClient, private toasterService: ToasterService) { }
-
   public getClaimantById(claimantId: number): Observable<ClaimantSO> {
     const claimantUrl = `http://localhost:8080/api/claimant/getClaimantById/${claimantId}`;
     return this.http.get<ClaimantSO>(claimantUrl)
@@ -35,16 +35,25 @@ export class ClaimantService {
     }));
   } 
 
-  public registerClaimant(claimantSO:ClaimantSO): Observable<number> {
-    console.log(123);
+  public registerClaimant(claimantSO:ClaimantSO): Promise<any> {
+    console.log("inside register claimant");
     const claimantUrl = `http://localhost:8080/api/claimant/registerClaimant`;
-    return this.http.post<number>(claimantUrl, claimantSO).pipe(catchError((err: any) => {
-      console.log(12);
-      this.toasterService.danger("ERROR", "The port you are trying to access cannot be reached.");
-      return EMPTY;
-    }));
+    return this.http.post(claimantUrl, claimantSO)
+    .toPromise()
+    .then(this.extractData)
+    .catch(this.handleErrorPromise)
   }
 
+  extractData(res: Response) {
+    this.toasterService.success("Success!", "Profile has been registered");
+    console.log(res);
+  }
+
+  handleErrorPromise (error: Response | any) {
+    // this.toasterService.danger("ERROR", error);
+    console.error(error.message || error);
+    return Promise.reject(error.message || error);
+  } 
   // updateClaimantPayment() {
 
   // }
