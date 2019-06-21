8368651efb48575084d084b106db40995ec69a2b
jmIbardaloza <josemariaibardaloza@yahoo.com.ph>
2019-06-21 17:22:30 +0800

    Update agency-portal-v2
    - Add interface for claimant-overview
    - Create survey-editor app

--- agency-portal/src/app/main/claimant-profile/claimant-profile.component.ts
+++ agency-portal/src/app/main/claimant-profile/claimant-profile.component.ts
@@ -5,6 +5,7 @@ import { ModalService } from '../../portal-services/modal.service';
 import { EditClaimantDetailsComponent } from '../edit-claimant-details/edit-claimant-details.component';
 import { EditClaimantAddressesComponent } from '../edit-claimant-addresses/edit-claimant-addresses.component';
 import { PortalService } from '../../portal-services/portal.service';
+import { FormBuilder } from '../../../../node_modules/@angular/forms';
 
 @Component({
   selector: 'app-claimant-profile',
@@ -13,14 +14,37 @@ import { PortalService } from '../../portal-services/portal.service';
 })
 export class ClaimantProfileComponent implements OnInit {
   claimantSO: ClaimantSO;
+  profileForm: any;
 
-  constructor(private route: ActivatedRoute, private modalService: ModalService, private claimantUsed: PortalService) { }
+  constructor(private route: ActivatedRoute, private modalService: ModalService, private claimantUsed: PortalService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-  //   this.route.queryParams.subscribe((params: Params) => {
-  //     this.claimantSO = JSON.parse(params.claimantSO) as ClaimantSO;
-  // });
     this.claimantSO = this.claimantUsed.claimantSO;
+
+    console.log(this.claimantSO)
+
+    this.profileForm = this.formBuilder.group({
+      ssn: [''],
+      alternateClaimantId: [''],
+      firstName: [''],
+      lastName: [''],
+      middleInitial: [''],
+      dateOfBirth: [new Date(2019, 1, 1)],
+      homePhone: [''],
+      mobilePhone: [''],
+      preferredOccupation: [''],
+      languagePreference: [''],
+      gender: [''],
+      educationLevel: [''],
+      race: [''],
+      ethnicity: [''],
+      addressLine1: [''],
+      addressLine2: [''],
+      city: [''],
+      state: [''],
+      zipCode: [''],
+      zipExt: [''],
+    });
   }
 
   openEditClaimantDetails(object: any){
