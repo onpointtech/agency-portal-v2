9b2124e60eb371a6325786c6524b1d966281eb9c
cidazc <joseazcarragaiv@gmail.com>
2019-07-16 14:59:52 +0800

    Update Script

--- external-app/custombuild.sh
+++ external-app/custombuild.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 ng build external-app --prod --output-hashing=none && cat dist/external-app/runtime-es2015.js dist/external-app/polyfills-es2015.js dist/external-app/scripts.js dist/external-app/main-es2015.js > preview/externalapp.js
+dist/external-app/styles.css > preview/styles.css
